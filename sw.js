e10f34ab04d5d47fe817d718179ddb5ec08b6088
Chris Woollard <cwoollard@gmail.com>
2026-09-09 11:44:30 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'local-developer-tools-v2.1.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './favicon.svg',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        if (new URL(event.request.url).origin === self.location.origin) {
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
